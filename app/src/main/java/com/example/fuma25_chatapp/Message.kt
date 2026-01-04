dcd2b3c949fc20f36111d3273d61c0be30b140d2
GreedyGobling <greedygobling@gmail.com>
2026-01-04 10:53:39 +0100

    update Message.kt

--- app/src/main/java/com/example/fuma25_chatapp/Message.kt
+++ app/src/main/java/com/example/fuma25_chatapp/Message.kt
@@ -1,6 +1,29 @@
 package com.example.fuma25_chatapp
 
-data class Message(var messageId: String = "", var sendorId: String = "", var name: String = "", var message: String = "", var time: Double){
+data class Message(
+    var messageId: String = "",
+    var userId: String = "",
+    var name: String = "",
+    var text: String = "",
+    var time: Double = 0.0){
     constructor():this("","","","",0.0)
 }
 // id for user, name for the ui part to show who sent message, message, and time for then the message was sent
+
+
+/* firebase database structure
+can be updated later
+
+users/userId
+    name: String
+    email: String
+
+chat-room/roomId
+    users-list: String
+
+chat-room/roomId/messages/messageId
+    user: String
+    text: String
+    userId: String
+
+*/
